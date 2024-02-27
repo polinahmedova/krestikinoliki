04fc1b9b8e50ea724fd4258cfcd37dff4a6300d7
polinahmedova <polinahmedova510@gmail.com>
2024-02-27 18:31:37 +0500

    home and rooms

--- krestikinolikigame/lib/app/modules/home/views/home_view.dart
+++ krestikinolikigame/lib/app/modules/home/views/home_view.dart
@@ -27,11 +27,44 @@ class PlayerNameScreen extends StatelessWidget {
       body: Container(
         color: Colors.blue[100],
         child: Center(
-          child: Padding(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.blue[300],
+            ),
+            width: 500,
+            height: 450,
             padding: const EdgeInsets.all(20.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  child: Text(
+                    'Добро пожаловать в игру "Крестики - нолики"!',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Text(
+                    'Для начала игры, пожалуйста, авторизуйтесь.',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
                 Container(
                   width: 600,
                   decoration: BoxDecoration(
@@ -47,13 +80,21 @@ class PlayerNameScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                SizedBox(height: 70),
                 Container(
+                  width: 200,
+                  height: 60,
                   decoration: BoxDecoration(
-                    color: Colors.blue[200],
+                    color: Colors.blue[300],
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Colors.blue[600],
+                    ),
                     onPressed: () {
                       String playerName = _controller.text;
 
@@ -85,8 +126,14 @@ class PlayerNameScreen extends StatelessWidget {
                         );
                       }
                     },
-                    child: Text('Войти'),
+                    child: Text(
+                      'Войти',
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
+                ),
+                SizedBox(
+                  height: 30,
                 ),
               ],
             ),
--- krestikinolikigame/lib/app/modules/rooms/views/rooms_view.dart
+++ krestikinolikigame/lib/app/modules/rooms/views/rooms_view.dart
@@ -21,13 +21,15 @@ class RoomsView extends GetView<RoomsController> {
         width: double.infinity,
         color: Colors.blue[100],
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Padding(
               padding: EdgeInsets.only(top: 30),
               child: Container(
+                height: 300,
+                width: 600,
                 decoration: BoxDecoration(
-                    color: Colors.blue[200],
+                    color: Colors.blue[300],
                     borderRadius: BorderRadius.circular(20)),
                 child: Column(
                   children: [
@@ -35,16 +37,17 @@ class RoomsView extends GetView<RoomsController> {
                       child: Flexible(
                         child: Padding(
                           padding: EdgeInsets.only(
-                              top: 15, left: 15, right: 15, bottom: 10),
+                              top: 60, left: 15, right: 15, bottom: 10),
                           child: RichText(
                             text: TextSpan(
                               children: <TextSpan>[
                                 TextSpan(
-                                    text: 'Выполнен вход под ником $nickname',
+                                    text:
+                                        'Вы успешно авторизовались под ником $nickname.',
                                     style: TextStyle(
                                       color: Colors.white,
                                       fontSize: 25,
-                                      fontWeight: FontWeight.w400,
+                                      fontWeight: FontWeight.w500,
                                     )),
                               ],
                             ),
@@ -55,11 +58,19 @@ class RoomsView extends GetView<RoomsController> {
                     SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
-                        Get.toNamed(Routes.CREATEROOM,
-                            arguments:
-                                nickname); // Navigate to create room page
+                        Get.toNamed(Routes.CREATEROOM, arguments: nickname);
+                        // Navigate to create room page
                       },
                       child: Text('Создать комнату'),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        backgroundColor: Colors.blue[600],
+                        padding: EdgeInsets.all(20), // increase padding
+                        textStyle:
+                            TextStyle(fontSize: 20), // increase font size
+                      ),
                     ),
                     SizedBox(height: 20),
                     ElevatedButton(
@@ -68,8 +79,20 @@ class RoomsView extends GetView<RoomsController> {
                             arguments:
                                 nickname); // Navigate to view available rooms page
                       },
-                      child: Text('Просмотреть доступные комнаты'),
+                      child: Text('Посмотреть доступные комнаты'),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        backgroundColor: Colors.blue[600],
+                        padding: EdgeInsets.all(20), // increase padding
+                        textStyle:
+                            TextStyle(fontSize: 20), // increase font size
+                      ),
                     ),
+                    SizedBox(
+                      height: 20,
+                    )
                   ],
                 ),
               ),
